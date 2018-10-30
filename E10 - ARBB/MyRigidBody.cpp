d95ce78e8b83ec4e69c584b55c188cb8165575f8
Cam Ostrom <cxo1192@g.rit.edu>
2018-10-30 00:18:52 -0400

    E10

--- E10 - ARBB/MyRigidBody.cpp
+++ E10 - ARBB/MyRigidBody.cpp
@@ -85,8 +85,44 @@ void MyRigidBody::SetModelMatrix(matrix4 a_m4ModelMatrix)
 	m_m4ToWorld = a_m4ModelMatrix;
 	
 	//your code goes here---------------------
+	vector3 corners[8];
+
+	//gen corners in local
+	corners[0] = m_v3MinL;  
+	corners[1] = vector3(m_v3MaxL.x, m_v3MinL.y, m_v3MinL.z);
+	corners[2] = vector3(m_v3MinL.x, m_v3MaxL.y, m_v3MinL.z);
+	corners[3] = vector3(m_v3MaxL.x, m_v3MaxL.y, m_v3MinL.z);
+	
+	corners[4] = vector3(m_v3MinL.x, m_v3MinL.y, m_v3MaxL.z);
+	corners[5] = vector3(m_v3MaxL.x, m_v3MinL.y, m_v3MaxL.z);
+	corners[6] = vector3(m_v3MinL.x, m_v3MaxL.y, m_v3MaxL.z);
+	corners[7] = m_v3MaxL;
+	
+
+	for (uint i =0; i < 8; ++i)//convert corners to world
+	{
+		corners[i] = vector3(m_m4ToWorld * vector4(corners[i], 1.0f));
+	}
+
+
+	//starting max/min
 	m_v3MinG = m_v3MinL;
 	m_v3MaxG = m_v3MaxL;
+
+	//find true max/min
+	for (size_t i = 1; i < 8; ++i)
+	{
+		if (m_v3MaxG.x < corners[i].x) m_v3MaxG.x = corners[i].x;
+		else if (m_v3MinG.x > corners[i].x) m_v3MinG.x = corners[i].x;
+
+		if (m_v3MaxG.y < corners[i].y) m_v3MaxG.y = corners[i].y;
+		else if (m_v3MinG.y > corners[i].y) m_v3MinG.y = corners[i].y;
+
+		if (m_v3MaxG.z < corners[i].z) m_v3MaxG.z = corners[i].z;
+		else if (m_v3MinG.z > corners[i].z) m_v3MinG.z = corners[i].z;
+	}
+
+	
 	//----------------------------------------
 
 	//we calculate the distance between min and max vectors
